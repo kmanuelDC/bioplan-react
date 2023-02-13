2eb2c4a996da8365e48e8d27bfd1cf0dd7da8e22
kmanuelDC <kmanuel.diazcalua@gmail.com>
2023-02-13 13:28:24 -0500

    initial proyecto

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { HomePage } from './pages/home/HomePage';
+
+function App() {
+  return (
+    <HomePage></HomePage>
+  );
+}
+
+export default App;
---
+++ src/pages/home/HomePage.js
@@ -0,0 +1,15 @@
+import React, { memo } from 'react'
+
+
+
+export const HomePage = memo(() => {
+
+
+
+    return (
+        <div>
+            HOLA MUNDO
+
+        </div>
+    )
+})
