e27cc369f29606841837c82716c28ebe3305c8b3
HaroldUCSM <haroldosorio32@gmail.com>
2023-02-15 18:48:42 -0500

    Se realizo el desarrollo responsivo del navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,17 +4,91 @@ import IconFacebook from "../../assets/icons/Facebook.png";
 import IconInstagram from "../../assets/icons/Instagram.png";
 import IconYoutube from "../../assets/icons/Youtube.png";
 import Button from "@mui/material/Button";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+const drawerWidth = 240;
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
 
 const Navbar = () => {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   return (
-    <header className="navbar">
+    <header className="header">
       <div className="navbar-top-container">
         <div className="container-logo">
           <a href="">
-            <img src={LogoBioplan} alt="" />
+            <img src={LogoBioplan} alt="" className="LogoBioplan" />
           </a>
         </div>
         <div className="container-icons">
@@ -46,20 +120,100 @@ const Navbar = () => {
           </a>
         </div>
       </div>
-      <AppBar
-        position="relative"
-        color="primary"
-        className="navbar-bot-container"
-      >
-        <Toolbar className="conatiner-button">
-          <Button color="inherit">INICIO</Button>
-          <Button color="inherit">PROYECTOS</Button>
-          <Button color="inherit">CONVOCATORIAS</Button>
-          <Button color="inherit">BLOG</Button>
-          <Button color="inherit">CÓNOCENOS</Button>
-          <Button color="inherit">CONTÁCTANOS</Button>
-        </Toolbar>
-      </AppBar>
+      <div className="navbar-bot-container">
+        <AppBar position="relative" color="primary" className="">
+          <Toolbar className="conatiner-button">
+            <Button color="inherit">INICIO</Button>
+            <Button color="inherit">PROYECTOS</Button>
+            <Button color="inherit">CONVOCATORIAS</Button>
+            <Button color="inherit">BLOG</Button>
+            <Button color="inherit">CÓNOCENOS</Button>
+            <Button color="inherit">CONTÁCTANOS</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+
+      <div className="navbar-bot-responsive">
+        <Box sx={{ display: "flex" }}>
+          <CssBaseline />
+          <AppBar
+            position="absolute"
+            open={open}
+            style={{ backgroundColor: "#136734" }}
+          >
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                edge="start"
+                sx={{ mr: 2, ...(open && { display: "none" }) }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" noWrap component="div">
+                <a className="img-logoBioplan">
+                  <img src={LogoBioplan} alt="" className="LogoBioplan" />
+                </a>
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            sx={{
+              width: drawerWidth,
+              flexShrink: 0,
+              "& .MuiDrawer-paper": {
+                width: drawerWidth,
+                boxSizing: "border-box",
+              },
+            }}
+            variant="persistent"
+            anchor="left"
+            open={open}
+          >
+            <DrawerHeader>
+              <IconButton onClick={handleDrawerClose}>
+                {theme.direction === "ltr" ? (
+                  <ChevronLeftIcon />
+                ) : (
+                  <ChevronRightIcon />
+                )}
+              </IconButton>
+            </DrawerHeader>
+            <Divider />
+            <List>
+              {["Inicio", "Proyectos", "Convocatorias", "Blog", "Conócenos", "Contáctanos"].map(
+                (text, index) => (
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                    </ListItemButton>
+                  </ListItem>
+                )
+              )}
+            </List>
+            <Divider />
+            <List>
+              {["All mail", "Trash", "Spam"].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Drawer>
+          <Main open={open}>
+            <DrawerHeader />
+          </Main>
+        </Box>
+      </div>
     </header>
   );
 };
