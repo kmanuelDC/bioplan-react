6def6acb1c9818a0bb0617788466bb75ff40f2ed
HaroldUCSM <haroldosorio32@gmail.com>
2023-02-17 22:32:08 -0500

    Se realizo el blog en el homePage

---
+++ src/components/cardComponent/CardComponent.jsx
@@ -0,0 +1,12 @@
+import CardPrimary from "../card/Card";
+import "./cardComponent.css";
+
+export default function CardComponent() {
+  return (
+    <section className="section-cards">
+      <CardPrimary descripcion="" fecha="" className="CardPrimary" />
+      <CardPrimary descripcion="" fecha="" className="CardPrimary" />
+      <CardPrimary descripcion="" fecha="" className="CardPrimary" />
+    </section>
+  );
+}
