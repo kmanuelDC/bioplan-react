0365453fda07af4d839d101d822c7808f5cd6ea6
kmanuelDC <kmanuel.diazcalua@gmail.com>
2023-02-25 20:57:21 -0500

    fix warning

--- src/components/lista/Lista.js
+++ src/components/lista/Lista.js
@@ -2,8 +2,8 @@ import Linkbtn from "../btn/link-ps";
 const Lista = (props) => {
   return (
     <ul id="sublista" className="p-0">
-      {props.items?.map((item) => (
-        <li className="list-group-item my-4">
+      {props.items?.map((item,i) => (
+        <li key={`sublist-${i}`} className="list-group-item my-4">
           {item.icon ? <i className="pr-3">{item.icon}</i> : ""}
 
           {item.url ? (
