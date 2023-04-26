9589af0b90164f962a80a1fba38823757553e34c
Arturo Enriquez <henriquezc15_2@unc.edu.pe>
2023-04-25 21:24:45 -0500

    12

--- src/components/Mapita/Mapita.jsx
+++ src/components/Mapita/Mapita.jsx
@@ -1,4 +1,17 @@
+import React, {Component} from "react";
+import GoogleMaps from "simple-react-google-maps";
+// import default class Maps extends Component {
+//     render(){
+//         ret
+//     }
+// }
+
+
 import "./Mapita.css"
+
+import React from "react"
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 
 const Mapita = () => {
@@ -11,10 +24,10 @@ const Mapita = () => {
     */
     return (
         <>
-            <iframe
+            {/* <GoogleMaps
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.6644704589007!2d-78.51924668594596!3d-7.164731672287779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91b25b01c6751bd9%3A0x15c42c743b32e69d!2sJr.%20Romero%20389%2C%20Cajamarca%2006002!5e0!3m2!1ses!2spe!4v1678123995401!5m2!1ses!2spe"
                 width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
+                referrerpolicy="no-referrer-when-downgrade"></iframe> */}
         </>
     );
 };
